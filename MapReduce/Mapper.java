589af433f8bf063d1fedf640737d48a1b12b25a8
ErikaLinda <erika.zogla@gmail.com>
2017-04-24 17:46:24 +0400

    The structure as outlined in tutorials

---
+++ MapReduce/Mapper.java
@@ -0,0 +1,16 @@
+package MapReduce;
+
+public class Mapper{
+
+/*steps to implement with paralel streams:
+
+	receive input 
+
+	tokenize input
+
+	map the tokens to key-value pairs
+
+	sort the key value pairs
+*/
+	
+}
---
+++ MapReduce/Reducer.java
@@ -0,0 +1,14 @@
+package MapReduce;
+
+public class Reducer{
+
+/*steps to implement:
+
+	receive input from the Mapper class
+
+	search matching key value pairs
+
+	reduce the data to the required output
+*/
+	
+}
