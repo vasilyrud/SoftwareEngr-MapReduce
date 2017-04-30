d69e8a94a2c74bb833703b3f563630dc08210d4f
Vasily Rudchenko <vasilyrud@gmail.com>
2017-04-30 13:11:58 +0400

    Create Master class

---
+++ projectWithTests/src/impl/Master.java
@@ -0,0 +1,22 @@
+package impl;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.DoubleSupplier;
+
+public class Master {
+    
+    
+    public Master() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Master runs");
+    }
+
+
+}
+
+
+
---
+++ projectWithTests/src/impl/WorkerInfo.java
@@ -0,0 +1,15 @@
+package impl;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.DoubleSupplier;
+
+public class WorkerInfo {
+    
+    
+    public WorkerInfo() {
+
+    }
+
+
+}
