fbfa8a85371480a408b75b9e59912c5496ad6bba
Vasily Rudchenko <vasilyrud@gmail.com>
2017-05-01 11:35:59 +0400

    Delete Workers since Thread Pool will be used instead

--- projectWithTests/src/impl/Master.java
+++ projectWithTests/src/impl/Master.java
@@ -9,6 +9,9 @@ public class Master {
     
     private int num_cores;
 
+    // Use ExecutorService to create a thread pool with num_cores threads
+    // Somehow keep track of threads nad what they are up to
+
     private Master() {
         this.num_cores = Runtime.getRuntime().availableProcessors();
     }
@@ -19,6 +22,10 @@ public class Master {
 
     public static Master getInstance() {
         return MasterHolder.INSTANCE;
+    }
+
+    public int returnNumCores() {
+        return num_cores;
     }
 }
 
