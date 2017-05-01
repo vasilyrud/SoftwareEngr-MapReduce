5b9c3e95387b6183caba49c1068e22fac85d63fe
Vasily Rudchenko <vasilyrud@gmail.com>
2017-05-01 10:39:48 +0400

    Make Master singleton

--- projectWithTests/src/impl/Master.java
+++ projectWithTests/src/impl/Master.java
@@ -4,14 +4,21 @@ import java.util.*;
 import java.util.stream.*;
 import java.util.function.DoubleSupplier;
 
+// Singleton because there is only one Master
 public class Master {
     
     private int num_cores;
 
-    public Master() {
+    private Master() {
         this.num_cores = Runtime.getRuntime().availableProcessors();
     }
+
+    private static class MasterHolder {
+        private static final Master INSTANCE = new Master();
+    }
+
+    public static Master getInstance() {
+        return MasterHolder.INSTANCE;
+    }
 }
-
-
 
--- projectWithTests/src/impl/RunMapReduce.java
+++ projectWithTests/src/impl/RunMapReduce.java
@@ -6,6 +6,8 @@ import java.util.function.DoubleSupplier;
 
 public class RunMapReduce {
     public static void main(String[] args) {
+        Master master = Master.getInstance();
+
         System.out.print("Started main");
     }
 }
