b40ded053612881d1d33e85e94ee3d384705079a
elo239 <elo239@nyu.edu>
2017-05-06 23:14:06 +0400

    Refactoring MapClass

--- projectWithTests/src/impl/MapClass.java
+++ projectWithTests/src/impl/MapClass.java
@@ -20,7 +20,6 @@ public class MapClass implements Runnable {
 
     private int map_id;
     private Long start;
-    private Long end;
     private int total;
 
     private int word_index_i;
@@ -35,7 +34,6 @@ public class MapClass implements Runnable {
     public MapClass(int id, Long start, Long end) {
         this.map_id = id;
         this.start = start;
-        this.end = end;
         this.total = (int)(end - start);
         this.buffer = new byte[total];
         this.word_index_i = -1;
@@ -43,66 +41,25 @@ public class MapClass implements Runnable {
         this.countries_count = new HashMap<String, Long>();
     }
 
-    private boolean find_word_index(String word) {
-        if (Master.getInstance().countries_indices.containsKey(word)) {
-            word_index_i = Master.getInstance().countries_indices.get(word).get(0);
-            word_index_j = Master.getInstance().countries_indices.get(word).get(1);
-            return true;
-        }
-        return false;
-    }
-
-    private int arrays_equal(List<String> array_1, List<String> array_2) {
-        if (array_1.size() != array_2.size()) {
-            return -1;
-        }
-        for (int i = 0; i < array_1.size(); i++) {
-            if (!array_1.get(i).equals(array_2.get(i))) {
-                return -1;
-            }
-        }
-        return 1;
-    }
-
+    // This is where the Map runs
     public void run() {
-        // This is where the Map runs
-        System.out.print("Starting map with id: ");
-        System.out.print(map_id);
-        System.out.println("");
-
+        System.out.printf("Starting map with id: %d.%n", map_id);
         Long startTime = System.currentTimeMillis();
 
-        try {
-            this.file = new RandomAccessFile(Master.getInstance().main_file_path, "r");
-        } catch (FileNotFoundException e) {
-            System.out.println("Inside map: FileNotFoundException occured");
-        } catch (NullPointerException e) {
-            System.out.println("CAUGHT NullPointerException");
-        }
-
+        // retrieve the file block to be processed
+        this.fileInit(start, buffer);
 
         char curr_char = '0';
         int byte_counter = 0;
         int counter = 0;
         StringBuilder curr_word = new StringBuilder(30);
         String string_word;
+        List<String> potential_match;
         ArrayList<String> final_string;
         int expected_phrase_size = 0;
 
-        // Find start of segment
-        try {
-            file.seek(start);
-            file.read(buffer);
-        } catch (IOException e) {
-            System.out.println("Inside map: IOException occured");
-        } catch (NullPointerException e) {
-            System.out.println("CAUGHT NullPointerException");
-        }
-
-        while (true) {
-            if (counter >= total) {
-                break;
-            }
+    
+        while (counter < total) {
 
             // Iterate through file bytes until an uppercase character is reached
             do {
@@ -119,17 +76,17 @@ public class MapClass implements Runnable {
             }
             
             // loop through all countries to check if the word matches with any of the first words in the sub-names
+            // if it does identify potential match country name
             string_word = curr_word.toString();
-            if (find_word_index(string_word) == false) {
-                continue;
-            }
-            
-            // if it does, and if there are more words in the country sub-name, check next bytes in the file
-            if (Master.getInstance().countries_array.get(word_index_i).get(word_index_j).size() > 1) {
+            if (!find_word_index(string_word)) continue;   
+            else  potential_match = Master.getInstance().countries_array.get(word_index_i).get(word_index_j);
+
+            // if there are more words in the country sub-name, then check next bytes in the file
+            if (potential_match.size() > 1) {
                 expected_phrase_size = 0;
-                for (int p = 1; p < Master.getInstance().countries_array.get(word_index_i).get(word_index_j).size(); p++) {
+                for (int p = 1; p < potential_match.size(); p++) {
                     expected_phrase_size += 1; // account for spaces
-                    expected_phrase_size += Master.getInstance().countries_array.get(word_index_i).get(word_index_j).get(p).length();
+                    expected_phrase_size += potential_match.get(p).length();
                 }
                 curr_word.append(curr_char); // append the one from previous loop
                 for (int l = 0; counter < total && l < expected_phrase_size; l++) {
@@ -140,13 +97,13 @@ public class MapClass implements Runnable {
 
             // check if the string matches expected country name
             final_string = new ArrayList<String>(Arrays.asList(curr_word.toString().split(" ")));
-            if (arrays_equal(final_string, Master.getInstance().countries_array.get(word_index_i).get(word_index_j)) == -1) {
+            if (arrays_equal(final_string, potential_match) == -1) {
                 continue;
             }
 
             // if there is a full match, iterate in a local HashMap the country name (i.e. first subname)
             String hash_name = "";
-            for (String s : Master.getInstance().countries_array.get(word_index_i).get(word_index_j)) {
+            for (String s : potential_match) {
                 hash_name += s + " ";
             }
             hash_name = hash_name.substring(0, hash_name.length() - 1);
@@ -188,6 +145,50 @@ public class MapClass implements Runnable {
         Long stopTime = System.currentTimeMillis();
         Long elapsedTime = stopTime - startTime;
         System.out.println("Timing map with id: " + map_id + ", milli-seconds ran: " + elapsedTime.floatValue());
+    }
+
+    private void fileInit(Long start, byte [] buffer){
+        // read file
+        try {
+            this.file = new RandomAccessFile(Master.getInstance().main_file_path, "r");
+        } catch (FileNotFoundException e) {
+            System.out.println("Inside map: FileNotFoundException occured");
+        } catch (NullPointerException e) {
+            System.out.println("CAUGHT NullPointerException");
+        }
+
+        // Find start of segment and read the block
+        try {
+            file.seek(start);
+            file.read(buffer);
+        } catch (IOException e) {
+            System.out.println("Inside map: IOException occured");
+        } catch (NullPointerException e) {
+            System.out.println("CAUGHT NullPointerException");
+        }
+    }
+
+    /****** HELPERS *****/
+
+    private boolean find_word_index(String word) {
+    if (Master.getInstance().countries_indices.containsKey(word)) {
+        word_index_i = Master.getInstance().countries_indices.get(word).get(0);
+        word_index_j = Master.getInstance().countries_indices.get(word).get(1);
+        return true;
+    }
+    return false;
+    }
+
+    private int arrays_equal(List<String> array_1, List<String> array_2) {
+        if (array_1.size() != array_2.size()) {
+            return -1;
+        }
+        for (int i = 0; i < array_1.size(); i++) {
+            if (!array_1.get(i).equals(array_2.get(i))) {
+                return -1;
+            }
+        }
+        return 1;
     }
 }
 
