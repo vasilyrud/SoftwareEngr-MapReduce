1600e2d0e5b939ac90c72c48765fa2c752c29d20
elo239 <elo239@nyu.edu>
2017-05-10 16:51:56 +0400

    Retrun to previous settings

--- projectWithTests/src/impl/RunMapReduce.java
+++ projectWithTests/src/impl/RunMapReduce.java
@@ -7,32 +7,32 @@ import java.util.function.DoubleSupplier;
 public class RunMapReduce {
     public static void main(String[] args) {
 
-        // get the source file path
-        // Scanner reader = new Scanner(System.in);
-        // System.out.println("Enter your source file (XML) path: ");
-        // String src_file_path = reader.nextLine();
+        //get the source file path
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your source file (XML) path: ");
+        String src_file_path = reader.nextLine();
 
-        // // get the search file path
-        // System.out.println("Enter your search file (csv) path: ");
-        // String search_file_path = reader.nextLine();
+        // get the search file path
+        System.out.println("Enter your search file (csv) path: ");
+        String search_file_path = reader.nextLine();
 
-        // // get the map block size
-        // System.out.println("Enter desired block size (MB): ");
-        // int block_size = reader.nextInt();
+        // get the map block size
+        System.out.println("Enter desired block size (MB): ");
+        int block_size = reader.nextInt();
 
 
         /*********************************************
             ADJUSTABLE SETTINGS
         *********************************************/
             // 616 MB wiki xml source file (Windows)
-            String src_file_path = "C:/Users/USER/Documents/Software Engineering/SoftwareEngr-MapReduce/smallWiki.xml";
+            //String src_file_path = "C:/Users/USER/Documents/Software Engineering/SoftwareEngr-MapReduce/smallWiki.xml";
             // 58 GB wiki xml file on hard drive (Mac)
             //String file_path = "/Volumes/Samsung_T3/enwiki-20170420-pages-articles.xml";
 
-            String search_file_path = "data/AllCountries.csv";
+            //String search_file_path = "data/AllCountries.csv";
 
             // block size in MB for splititng up the file
-            int block_size = 4;
+            //int block_size = 50;
         /*******************************************/
         
         // standard map output setting
